acc7c2e47dc506631295b2e240000e51915aa4b6
MengsrunNit <117285282+MengsrunNit@users.noreply.github.com>
2023-12-05 12:43:20 -0500

    final push

--- src/Test/AngleStructureComparatorTest.java
+++ src/Test/AngleStructureComparatorTest.java
@@ -1,76 +1,79 @@
 package Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
-
 import java.util.Map;
 import java.util.Set;
 import geometry_objects.angle.*;
 import geometry_objects.angle.comparators.AngleStructureComparator;
-
 import org.junit.jupiter.api.Test;
-
 import exceptions.FactException;
 import geometry_objects.points.*;
-
 import geometry_objects.Segment;
 import geometry_objects.points.PointDatabase;
 import input.InputFacade;
 import input.components.FigureNode;
-
 class AngleStructureComparatorTest extends AngleStructureComparator{
-
 	@Test
-	void compareTest1() {
+	void compareTest1() throws FactException {
 		
-		Point pointA = new Point("A", 0, 0); 
-		Point pointB = new Point("B", 3, 0); 
-		Point pointC = new Point("C", 6, 0); 
-		Point pointD = new Point("D", 3, 3); 
-		Point pointE = new Point("E", 6, 6); 
-		 
-		Segment segAC = new Segment(pointA, pointC); 
+		//
+		//           E
+		//          /
+		//         /
+		//        /
+		//       /
+		//      D
+		//     /
+		//    /
+		//   /
+		//  /
+		// A------B-----C
+		//
+		
+		Point pointA = new Point("A", 0, 0);
+		Point pointB = new Point("B", 3, 0);
+		Point pointC = new Point("C", 6, 0);
+		Point pointD = new Point("D", 3, 3);
+		Point pointE = new Point("E", 6, 6);
+		
+		Segment segAC = new Segment(pointA, pointC);
 		Segment segAB = new Segment(pointA, pointB);
 		
-		Segment segAD = new Segment(pointA, pointD); 
+		Segment segAD = new Segment(pointA, pointD);
 		Segment segAE = new Segment(pointA, pointE);
 		
-		Angle angleLeft= null;
-		try {
-			angleLeft = new Angle(segAD, segAB );
-		} catch (FactException e) {
-			System.out.print("LeftAngle");
-			
-		} 
-		Angle angleRight = null;
-		try {
-			angleRight = new Angle(segAC, segAE);
-		} catch (FactException e) {
-			System.out.print("RightAngle");
-		} 
+		Angle angleLeft= new Angle(segAD, segAB);
+		Angle angleRight = new Angle(segAC, segAE);
 		
 		assertEquals(compare(angleLeft, angleRight), -1);
 		
 		
 		
-		Angle angleLeft1 = null; 
-		try {
-			angleLeft1 = new Angle(segAE, segAB );
-		} catch (FactException e) {
-			System.out.print("LeftAngle1");
-		} 
-		Angle angleRight1 = null; 
-		try {
-			angleRight1 = new Angle(segAD, segAB );
-		} catch (FactException e) {
-			System.out.print("LeftAngle1");
-		} 
+		Angle angleLeft1 = new Angle(segAE, segAB);
+		Angle angleRight1 = new Angle(segAD, segAB );;
+		
 		assertEquals(compare(angleLeft1, angleRight1), 1);
 		assertEquals(compare(angleRight1, angleLeft1), -1);
 		
 	}
 	@Test
-	void comapreTest2() {
+	void comapreTest2() throws FactException {
+		
+		//
+		// B         D
+		// |        /
+		// |       /
+		// |      /
+		// |     /
+		// E    C
+		// |   /
+		// |  /
+		// | /
+		// |/
+		// A---------F
+		//
+		
+		//Building figure shown above
+		
 		Point A = new Point("A", 0, 0);
 		Point B = new Point("B", 0, 10);
 		Point C = new Point("C", 5, 5);
@@ -84,43 +87,29 @@ class AngleStructureComparatorTest extends AngleStructureComparator{
 		Segment AE = new Segment(A, E);
 		Segment AF = new Segment(A, F);
 		
-		Angle BAD = null;
-		try {
-			BAD = new Angle(AB, AD);
-		} catch (FactException e) {
-			System.out.println("Angle BAD");
-		}
-		Angle EAC = null;
-		try {
-			EAC = new Angle(AE, AC);
-		} catch (FactException e) {
-			System.out.println("Angle EAC");
-		}
-		Angle BAF = null;
-		try {
-			BAF = new Angle(AB, AF);
-		} catch (FactException e) {
-			System.out.println("Angle BAF");
-		}
-		Angle EAD = null;
-		try {
-			EAD = new Angle(AE, AD);
-		} catch (FactException e) {
-			System.out.println("Angle BAD");
-		}
-		Angle BAC = null;
-		try {
-			BAC = new Angle(AB, AC);
-		} catch (FactException e) {
-			System.out.println("Angle BAC");
-		}
+		Angle BAD = new Angle(AB, AD);
+		Angle EAC = new Angle(AE, AC);
+		Angle BAF = new Angle(AB, AF);
+		Angle EAD = new Angle(AE, AD);
+		Angle BAC = new Angle(AB, AC);
+		Angle DAF = new Angle(AD, AF);
+		Angle CAF = new Angle(AC, AF);
 		
+		//Checking angles with different rays
 		assertEquals(compare(BAD, BAF), Integer.MAX_VALUE);
+		assertEquals(compare(BAD, DAF), Integer.MAX_VALUE);
+		
+		//Checking the unknown difference case
 		assertEquals(compare(BAC, EAD), 0);
+		assertEquals(compare(EAD, BAC), 0);
+		
+		//Left is larger
 		assertEquals(compare(BAD, EAC), 1);
 		assertEquals(compare(BAC, EAC), 1);
+		
+		//Right is larger
 		assertEquals(compare(EAC, BAC), -1);
 		assertEquals(compare(EAC, BAD), -1);
+		assertEquals(compare(CAF, DAF), -1);
 	}
-
 }
--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -32,11 +32,19 @@ public class TriangleIdentifier
 		//Get your three segments for the triangles
 		for (Segment s1 : _segments.values()) {
 			for (Segment s2 : _segments.values()) {
-				//Weed out
+				
+				//Trying to weed out cases where a triangle cannot be formed
 				Point p = s2.sharedVertex(s1);
 				if (p != null && !(s1.HasSubSegment(s2) || s2.HasSubSegment(s1))) {
+					
 					for (Segment s3 : _segments.values()) {	
+						
+						//Makes sure the last segment connects the other two at their other points
+						//Cannot be collinear
+						
 						if(s3.has(s1.other(p)) && s3.has(s2.other(p)) && !(s3.isCollinearWith(s1))) {
+							
+							//Makes list of segments to be put into the triangle constructor
 							List<Segment> t = new ArrayList<Segment>();
 							t.add(s1);
 							t.add(s2);
